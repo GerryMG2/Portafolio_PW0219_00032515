23c172c021072339da081dba6da1a68aed4ea493
Tu Nombre <you@example.com>
2019-10-08 14:50:26 -0600

    labo7

---
+++ Laboratorios/labo7/main.js
@@ -0,0 +1,46 @@
+var carnet = document.getElementById("carnet_field");
+var horario = document.getElementById("schedule_field");
+var check = document.getElementById("late_switch");
+var btn = document.getElementById("submit_btn");
+
+let carnet_validate = new RegExp("[0-9]{8}");
+let tabla = document.getElementById("table_body");
+
+let addStudent = (carnet,horario, tarde) => {
+    let new_row = document.createElement("tr");
+    let date = new Date();
+
+    new_row.className = "Table_active";
+
+    new_row.innerHTML = `<th  scope="row">${carnet}  </th> 
+    <td>${horario}</td>
+    <td>${date}</td>
+    <td>${tarde1(tarde)}</td>
+    
+    
+    `
+
+    tabla.appendChild(new_row);
+}
+btn.addEventListener("click", () =>{
+    let texto_carnet = carnet.value;
+    let horario_texto = horario.options[horario.selected];
+    let late = check.checked;
+    console.log(texto_carnet)
+    if(carnet_validate.test(texto_carnet)){
+        console.log("validado")
+        addStudent(texto_carnet,horario_texto,late);
+    }else{
+
+    }
+    
+})
+
+function tarde1(tarde){
+    if(tarde){
+        return "tarde";
+    }
+    else{
+        return "a tiempo";
+    }
+}
